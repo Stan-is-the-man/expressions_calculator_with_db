14a2549b8cb8e44358c7e157864400ae69f981ff
Stanislav Yanakiev <stan5050@gmail.com>
2023-05-07 19:18:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import re
+import time
+import psycopg2
+
+
+def factorial(any_integer):
+    the_result = 1
+    for num in range(1, any_integer + 1):
+        the_result *= num
+    return the_result
+
+
+def calculate_execution_time_in_milliseconds(func, *args):
+    start_time = time.perf_counter()
+    func(*args)
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    return execution_time
+
+
+def data_to_db(the_action, the_result, the_total_time):
+    connection = psycopg2.connect(
+        host='localhost',
+        database='expressions_calculator_with_db',
+        user='postgres',
+        password='1123QwER'
+    )
+    cursor_object = connection.cursor()
+    cursor_object.execute(
+        """CREATE TABLE IF NOT EXISTS calculator_statistics (    
+        operands VARCHAR(200),
+        results FLOAT,   
+        execution_time FLOAT
+    )"""
+    )
+    # connection.commit()
+
+    cursor_object.execute("""
+    INSERT INTO calculator_statistics(operands, results, execution_time)
+    VALUES(%s, %s, %s)
+    """, (the_action, the_result, the_total_time))
+    connection.commit()
+
+
+def symbol_count(the_expression):
+    count = 0
+    for char in the_expression:
+        if not char.isspace():
+            count += 1
+    if count <= 128:
+        return True
+
+
+while True:
+    expression = input("Enter a mathematical expression to be calculated or '!' for factorial. "
+                       "Type 'exit' to quit:\n")
+    if expression == 'exit':
+        break
+    if expression == "!":
+        try:
+            the_number = int(input("Please enter just ONE INTEGER number:\n"))
+            res = factorial(the_number)
+            exec_time = calculate_execution_time_in_milliseconds(factorial, the_number)
+            data_to_db(the_number, res, exec_time)
+            print(res)
+        except ValueError:
+            print("The entered number should be an integer !!!\n")
+
+    else:
+        try:
+            result = eval(expression)
+            if symbol_count(expression):
+                exec_time = calculate_execution_time_in_milliseconds(eval, expression)
+                all_operands = ', '.join(re.findall(r'\d+(?:\.\d+)?', expression))
+                data_to_db(all_operands, result, exec_time)
+                print(result)
+            else:
+                print('Up to 128 symbols accepted, please try again')
+
+        except Exception as e:
+            print(f"Error in {e}")
